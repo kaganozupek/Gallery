30d43d35cca4d1192ad17f9c28894fb092cba17a
Kagan Ozupek <kagan.ozupek@vngrs.com>
2018-09-25 22:14:05 +0300

    Xcode 10 Support added

--- Sources/Camera/CameraView.swift
+++ Sources/Camera/CameraView.swift
@@ -171,17 +171,17 @@ class CameraView: UIView, UIGestureRecognizerDelegate {
     return button
   }
 
-  func makeFlashButton() -> TripleButton {
-    let states: [TripleButton.State] = [
-      TripleButton.State(title: "Gallery.Camera.Flash.Off".g_localize(fallback: "OFF"), image: GalleryBundle.image("gallery_camera_flash_off")!),
-      TripleButton.State(title: "Gallery.Camera.Flash.On".g_localize(fallback: "ON"), image: GalleryBundle.image("gallery_camera_flash_on")!),
-      TripleButton.State(title: "Gallery.Camera.Flash.Auto".g_localize(fallback: "AUTO"), image: GalleryBundle.image("gallery_camera_flash_auto")!)
-    ]
-
-    let button = TripleButton(states: states)
-
-    return button
-  }
+    func makeFlashButton() -> TripleButton {
+        let states: [TripleButton.ButtonState] = [
+            TripleButton.ButtonState(title: "Gallery.Camera.Flash.Off".g_localize(fallback: "OFF"), image: GalleryBundle.image("gallery_camera_flash_off")!),
+            TripleButton.ButtonState(title: "Gallery.Camera.Flash.On".g_localize(fallback: "ON"), image: GalleryBundle.image("gallery_camera_flash_on")!),
+            TripleButton.ButtonState(title: "Gallery.Camera.Flash.Auto".g_localize(fallback: "AUTO"), image: GalleryBundle.image("gallery_camera_flash_auto")!)
+        ]
+        
+        let button = TripleButton(states: states)
+        
+        return button
+    }
 
   func makeRotateButton() -> UIButton {
     let button = UIButton(type: .custom)
